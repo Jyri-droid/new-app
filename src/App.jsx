768ac5ebf0f19801454be310c16732a1c71f6758
Jyri Konttinen <62062679+Jyri-droid@users.noreply.github.com>
2024-05-16 13:38:07 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from './Menu';
 import Score from './pages/Score';
 import Settings from './pages/Settings';
