99c6fe150b88061a36f9aeed4cb92ef78cb08c13
Jyri Konttinen <62062679+Jyri-droid@users.noreply.github.com>
2024-11-21 17:29:43 +0200

    fix reports icon

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -4,12 +4,14 @@ import IconHourglassActive from './assets/IconHourglassActive';
 import IconHourglassDefault from './assets/IconHourglassDefault';
 import IconGearDefault from './assets/IconGearDefault';
 import IconGearActive from './assets/IconGearActive';
+import IconWaveformDefault from './assets/IconWaveformDefault';
+import IconWaveformActive from './assets/IconWaveformActive';
 
 const Menu = () => {
     const [active, setActive] = useState(0);
     const items = [
         {text: 'Activity', navigateTo: '/', defaultIcon: <IconHourglassDefault />, activeIcon: <IconHourglassActive />},
-        {text: 'Reports', navigateTo: '/reports', defaultIcon: <IconHourglassDefault />, activeIcon: <IconHourglassActive />},
+        {text: 'Reports', navigateTo: '/reports', defaultIcon: <IconWaveformDefault />, activeIcon: <IconWaveformActive />},
         {text: 'Settings', navigateTo: '/settings', defaultIcon: <IconGearDefault />, activeIcon: <IconGearActive />},
     ];
     return <div className='menu-container'>
